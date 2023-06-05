6491519090d987dbedb7159ba9cd1e74e9a011d8
Barsbekteam <barsbekbarsbek99@gmail.com>
2023-06-05 20:43:18 +0300

    fixes

---
+++ src/Store/testReduce.js
@@ -0,0 +1,22 @@
+import {createSlice, PayloadAction} from "@reduxjs/toolkit";
+
+const initialState = {
+    competition: [],
+    loader: false,
+}
+
+export const testSlice = createSlice({
+    name: 'test',
+    initialState,
+    reducers: {
+        getCompetitions(state, action){
+            state.competition = action.payload
+        },
+        getLoader(state, action){
+            state.loader = action.payload
+        }
+    }
+})
+
+export const {getCompetitions,getLoader} = testSlice.actions
+export default testSlice.reducer
